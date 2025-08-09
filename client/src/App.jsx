21ed400e8cd038b172f14786066effac1e2cf69a
Punnawithsut <punnawith05@gmail.com>
2025-08-09 17:01:43 +0700

    run test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,9 @@ import viteLogo from '/vite.svg'
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className="">
+      <h1 className='text-lg text-blue-700'>TAILWINDCSS TEST</h1>
+    </div>
   )
 }
 
