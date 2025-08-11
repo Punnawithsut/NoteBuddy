1baa3ac555b50f0a1b82c619ffe982e80a0a1b46
Punnawithsut <punnawith05@gmail.com>
2025-08-11 16:52:45 +0700

    initialize server, connect to mongoDB, create dummy file

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  //profilePic: {},
+  //bio: {},
+  createdAt:{ type: Date, default: Date.now }
+});
+
+export const userModel = mongoose.model("User", userSchema);
--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./libs/db.js";
 
 const app = express();
 const PORT = 5000;
+
+const mongodb_uri = process.env.MONGODB_URI;
+const database_name = process.env.MONGODB_NAME;
 
 app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Currently listen to ${PORT}`);
 });
+
+app.get("/api/status", (req, res) => {
+    res.json("Server Live Right now!");
+})
+
+connectDB(mongodb_uri, database_name);
